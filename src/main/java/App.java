1ec42f870bd7c9f896f765d20b2d01e10fe2ab63
Rispo3 <ltyutyunnikov74@gmail.com>
2023-01-08 14:27:29 +0300

    на проверку

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat first = (Cat) applicationContext.getBean(("cat"));
+        System.out.println(first.getMessage());
+
+        Cat second = (Cat) applicationContext.getBean(("cat"));
+        System.out.println(second.getMessage());
+
+        System.out.println(bean==bean2);
+        System.out.println(first==second);
     }
 }
